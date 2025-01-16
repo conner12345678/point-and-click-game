78e75f4214ac1cadd4b8883982bf705f289b4f11
conner12345678 <cehly065@west-mec.org>
2025-01-16 15:22:40 -0700

    worked on the map

---
+++ street.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		if Input.is_action_just_pressed("select"):
+			if handle_click($"Car"):
+				get_tree().change_scene_to_file("res://office.tscn")
+
+func handle_click(item):
+	var mouse_pos = get_global_mouse_position()
+	
+	# Get the global position and size of the duplicate
+	var item_pos = item.global_position
+	var size = item.get_texture().get_size() * item.get_global_scale()  # Assuming `duplicate` has a texture
+	
+	# Create a Rect2 using the duplicate's position and size
+	var rect = Rect2(item_pos - size * 0.5, size)
+	
+	# Check if the mouse is within the bounding box
+	return rect.has_point(mouse_pos)
